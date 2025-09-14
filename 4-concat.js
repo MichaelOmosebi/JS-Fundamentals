0e2d8d4a59baec3e317e4750146f08028daa7840
Michael Omosebi <omosebimichael@live.com>
2025-09-14 12:48:06 +0100

    re-submit concat task

--- 4-concat.js
+++ 4-concat.js
@@ -5,8 +5,8 @@
 
 if (process.argv.length == 2) {
     console.log(`${process.argv[2]} is undefined`);
-} else if (process.argv[2] === undefined || process.argv[3] === undefined) {
-    console.log(`${process.argv[2]}, ${process.argv[3]} is undefined`);
+// } else if (process.argv[2] === undefined || process.argv[3] === undefined) {
+//     console.log(`${process.argv[2]}, ${process.argv[3]} is undefined`);
 } else {
     console.log(process.argv[2] + " is " + process.argv[3]);
 }
